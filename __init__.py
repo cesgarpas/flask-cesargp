f1d0a183402ae4ada92b7ab6ac4899bf670e9181
César García <cgpcsjcmp@gmail.com>
2018-06-19 01:03:11 +0200

    Ignores

--- __init__.py
+++ __init__.py
@@ -3,9 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
+    return "Index"
+
+@app.route('/hello')
+def hello():
     return "Hello World"
+
+@app.route('/hola')
+def hola():
+    return "Hola Mundo"
 
 if __name__ == "__main__":
     app.run()
